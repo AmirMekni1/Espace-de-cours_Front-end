9b262b97e4f10881757f87ca423d41d2828ecc9a
AmirMekni1 <mekniamir09@gmail.com>
2023-12-04 16:15:53 +0100

    jdid

--- src/app/Authentification/V_Email-R_Mot-De-Passe/verification-email/verification-email.component.ts
+++ src/app/Authentification/V_Email-R_Mot-De-Passe/verification-email/verification-email.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { VerificationEmailService } from '../../../Email/Service/verification-email.service';
 import { ServiceAuthentificationService } from '../../Service/service-authentification.service';
 
 @Component({
